a202276683bcb83faa1b7808fbb0444001c3b05f
Rafay Chohan <138992589+Rafay-Chohan@users.noreply.github.com>
2023-07-08 20:40:44 +0500

    Update Db.sql

--- Db.sql
+++ Db.sql
@@ -25,19 +25,10 @@ Create Table fosterData(Rescue_ID int,Public_ID varchar(15) not NULL check(Publi
 insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Cat','BlinkA','Male',NULL,'Yes','Yes','Adoption',NULL);
 insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Cat','BlinkB','Male',NULL,'Yes','Yes','Foster',NULL);
 insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Cat','BlinkC','Male',NULL,'Yes','Yes','Sponsor',NULL);
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Cat','NekoA','Female',NULL,'Yes','Yes','Adoption',NULL);
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Cat','NekoB','Female',NULL,'Yes','Yes','Foster',NULL);
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Cat','NekoC','Female',NULL,'No','No','Sponsor',NULL);
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Dog','Kelsy','Female','2 years','Yes','Yes','Adoption',NULL);
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Dog','Jax','Male','1 year','Yes','Yes','Foster','2021-04-12');
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Dog','Merry','Female','4 months','Yes','Yes','Sponsor','2020-03-12');
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Dog','Pluto','Male','14 months','Yes','Yes','Adoption','2022-12-23');
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Dog','Bruno','Female',NULL,'Yes','Yes','Foster',NULL);
-insert into Rescues (Animal,Name,Gender,Age,Sterile,Vac_status,Status,rescued_Date) VALUES ('Dog','Cinamon','Male',NULL,'No','No','Sponsor',NULL);
 
 
-insert into Public (First_Name,Second_Name,Gender,DOB,CNIC,Phone,Type_Public) VALUES('RandomA','PublicA','Female','2002-06-27','35202-7586101-8','03162455583','Adoption Parent');
-insert into Public (First_Name,Second_Name,Gender,DOB,CNIC,Phone,Type_Public) VALUES('RandomB','PublicB','Male','2001-08-22','35202-7545101-8','03161655583','Sponsor');
+insert into Public (First_Name,Second_Name,Gender,DOB,CNIC,Phone,Type_Public) VALUES('RandomA','PublicA','Female','2002-06-27','35202-0000000-0','03162455583','Adoption Parent');
+
 
 
 insert into Projects (Project_Name,Area,Starting_Date,Ending_Date,Goal,Project_status) VALUES('TNVR','Valencia','2023-01-01',NULL,'Spay/Neuter stray dogs','Ongoing');
@@ -51,14 +42,4 @@ insert into Events (Event_Name,Venue,Event_Date,aboutEvent) VALUES('Halloween','
 insert into Events (Event_Name,Venue,Event_Date,aboutEvent) VALUES('Birthday Party','Shelter','2023-07-03','Birthday Party for fundrasing');
 
 insert into Management (First_Name,Second_Name,Gender,DOB,CNIC,Designation,Phone,Pass) VALUES('admin','admin','Male','2001-12-02','11111-1111111-1','Admin','03000000','123');
-
-
-select * from Rescues;
-select * from Management;
-select * from Projects;
-select * from Public;
-select * from Events;
-select * from EventPublicData;
-select * from RescuePublicData;
-select * from sponsorData;
-select * from fosterData;
+S
